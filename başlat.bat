4f45291896b65c1787d5e18e3d1b6ada921d6e94
katilmamioffical <akbalmuhammed668@gmail.com>
2025-06-26 23:53:29 +0300

    Create başlat.bat

---
+++ başlat.bat
@@ -0,0 +1,12 @@
+@echo off
+title Mami Kicker Bot
+cls
+
+echo Mami Kicker Bot baslatiliyor...
+echo -------------------------------
+node mami.js
+
+echo.
+echo [!] Bot durduruldu veya hata ile sonlandi.
+echo Devam etmek icin herhangi bir tusa basin...
+pause >nul
