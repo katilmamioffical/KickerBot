1953d23cde896141a298dec5acb86467ea8d0c3e
katilmamioffical <akbalmuhammed668@gmail.com>
2025-06-26 23:54:33 +0300

    Create kurulum.bat

---
+++ kurulum.bat
@@ -0,0 +1,11 @@
+@echo off
+title Mami Kicker Bot - Kurulum
+cls
+
+echo [KURULUM] Gerekli paketler yukleniyor...
+echo ----------------------------------------
+call npm install
+
+echo.
+echo Kurulum tamamlandi. Devam etmek icin bir tusa basin...
+pause >nul
