6573ee3d867c122b510fc3bf330c008fd636ba6f
katilmamioffical <akbalmuhammed668@gmail.com>
2025-06-27 13:47:46 +0300

    Update and rename config.json to config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Your Discord account token (keep this secret!)
+  token: "YOUR_DISCORD_TOKEN_HERE",
+
+  // Command prefix for the bot commands
+  prefix: "!!",
+
+  // Language setting: "en" for English, "tr" for Turkish
+  language: "en"
+};
