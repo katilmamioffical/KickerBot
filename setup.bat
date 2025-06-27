54b97dd62beda1a8d0b924d9098f6c004e2024a2
katilmamioffical <akbalmuhammed668@gmail.com>
2025-06-27 13:36:21 +0300

    Update and rename kurulum.bat to setup.bat

---
+++ setup.bat
@@ -0,0 +1,11 @@
+@echo off
+title Mami Kicker Bot - Setup
+cls
+
+echo [SETUP] Installing required packages...
+echo ----------------------------------------
+call npm install
+
+echo.
+echo Setup completed. Press any key to continue...
+pause >nul
