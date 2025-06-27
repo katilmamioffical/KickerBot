cb1561ea438c7746fef5a3e5dbbabae8949218b0
katilmamioffical <akbalmuhammed668@gmail.com>
2025-06-27 13:38:59 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo "Starting Mami Kicker Bot..."
+echo "-------------------------------"
+node mami.js
+
+echo
+read -p "[!] Bot has stopped or encountered an error. Press enter to continue..."
