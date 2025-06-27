0f807619a6a75922439db0f921f372afdb8164b0
katilmamioffical <akbalmuhammed668@gmail.com>
2025-06-27 13:37:15 +0300

    Update and rename baslat.bat to start.bat

---
+++ start.bat
@@ -0,0 +1,12 @@
+@echo off
+title Mami Kicker Bot
+cls
+
+echo Starting Mami Kicker Bot...
+echo -------------------------------
+node mami.js
+
+echo.
+echo [!] Bot has stopped or encountered an error.
+echo Press any key to continue...
+pause >nul
